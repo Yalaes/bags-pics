995c8e55df94cd60be6797b42b5d8da6ce5c3f2d
Yalaes <yalaes.utrigas@gmail.com>
2021-07-08 12:47:11 +0700

    maquette

---
+++ js/script.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+    // similar behavior as clicking on a link
+    window.location.href = "https://bags-pics.netlify.app/maquette.pdf";
+}
+
+//burger menu
+const burgerBtn = document.querySelector(".burgerBtn");
+const navBurger = document.querySelector(".nav-burger");
+
+burgerBtn.addEventListener("click", toggleStuff);
+
+navBurger.addEventListener("click", toggleStuff);
+
+function toggleStuff () {
+    burgerBtn.classList.toggle("open");
+    navBurger.classList.toggle("toggle");
+}
+
+//fade in observer
+const toFade = document.querySelectorAll(".container");
+const options = {
+    root: null
+}
+
+const observer = new IntersectionObserver(beInTouch, options);
+toFade.forEach(el => {
+    observer.observe(el);
+})
+
+function beInTouch(entries) {
+    entries.forEach(element => {
+        if (element.isIntersecting) {
+            element.target.classList.add("fade")
+        }
+    });
+}
